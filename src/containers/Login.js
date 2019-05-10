f63828bb9919b2eef4e6ceacffda56ce24a1674f
ZuoSyuanWang <zuosyuanwang@gmail.com>
2019-05-10 11:53:23 +0800

    Login with AWS Cognito

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,29 +1,36 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
+import React, { Component } from 'react'
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import './Login.css'
+import { Auth } from 'aws-amplify'
 
 export default class Login extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 
 		this.state = {
-			email: "",
-			password: ""
-		};
+			email: '',
+			password: '',
+		}
 	}
 
 	validateForm() {
-		return this.state.email.length > 0 && this.state.password.length > 0;
+		return this.state.email.length > 0 && this.state.password.length > 0
 	}
 
 	handleChange = event => {
 		this.setState({
-			[event.target.id]: event.target.value
-		});
+			[event.target.id]: event.target.value,
+		})
 	}
 
-	handleSubmit = event => {
-		event.preventDefault();
+	handleSubmit = async event => {
+		event.preventDefault()
+		try {
+			await Auth.signIn(this.state.email, this.state.password)
+			alert('Logged in')
+		} catch (e) {
+			alert(e.message)
+		}
 	}
 
 	render() {
@@ -32,31 +39,17 @@ export default class Login extends Component {
 				<form onSubmit={this.handleSubmit}>
 					<FormGroup controlId="email" bsSize="large">
 						<ControlLabel>Email</ControlLabel>
-						<FormControl
-							autoFocus
-							type="email"
-							value={this.state.email}
-							onChange={this.handleChange}
-						/>
+						<FormControl autoFocus type="email" value={this.state.email} onChange={this.handleChange} />
 					</FormGroup>
 					<FormGroup controlId="password" bsSize="large">
 						<ControlLabel>Password</ControlLabel>
-						<FormControl
-							value={this.state.password}
-							onChange={this.handleChange}
-							type="password"
-						/>
+						<FormControl value={this.state.password} onChange={this.handleChange} type="password" />
 					</FormGroup>
-					<Button
-						block
-						bsSize="large"
-						disabled={!this.validateForm()}
-						type="submit"
-					>
+					<Button block bsSize="large" disabled={!this.validateForm()} type="submit">
 						Login
 					</Button>
 				</form>
 			</div>
-		);
+		)
 	}
 }
