35937d6eb414c7d5e73a2aa421fdbc13b467d52a
ZuoSyuanWang <zuosyuanwang@gmail.com>
2019-05-14 00:29:41 +0800

    Create a signup page

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,15 @@ import { Route, Switch } from 'react-router-dom'
 import Home from './containers/Home'
 import NotFound from './containers/NotFound'
 import Login from './containers/Login'
-import AppliedRoute from "./components/AppliedRoute";
+import Signup from './containers/Signup'
+import AppliedRoute from './components/AppliedRoute'
 
 export default ({ childProps }) => (
-    <Switch>
-        <AppliedRoute path="/" exact component={Home} props={childProps} />
-        <AppliedRoute path="/login" exact component={Login} props={childProps} />
-        {/* Finally, catch all unmatched routes */}
-        <Route component={NotFound} />
-    </Switch>
+	<Switch>
+		<AppliedRoute path="/" exact component={Home} props={childProps} />
+		<AppliedRoute path="/login" exact component={Login} props={childProps} />
+		<AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+		{/* Finally, catch all unmatched routes */}
+		<Route component={NotFound} />
+	</Switch>
 )
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 import './Login.css'
 import { Auth } from 'aws-amplify'
 import LoaderButton from '../components/LoaderButton'
